cfb0b23a363063b7be80a253cc7ea7d1c217b9cb
Michael Niebauer <89568973+niebauer-michael@users.noreply.github.com>
2021-10-19 03:37:25 -0400

    Add files via upload
    friend request/reject/approve

---
+++ r_v05/include/script_add_request.php
@@ -0,0 +1,65 @@
+<?php
+  session_start();
+?>
+
+<?php
+
+if (isset($_SESSION["userName"])){
+
+    require_once 'script_db_connection.php';
+    require_once 'script_functions.php';
+    
+    $currentUser = $_SESSION["userName"];
+    $requestor_name = $_GET['requestor_name'];
+
+    echo $currentUser;
+    echo $requestor_name;
+
+
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+
+        // check friendship status
+        $isCurrentFriend = false;
+    
+        $queryRelationship = "SELECT * FROM relationship_table";
+        // if results
+        if ($resultRelationship = $conn->query($queryRelationship)) {
+          // loop thru each row of table
+          while ($rowRelationship = $resultRelationship->fetch_assoc()) {
+            if($currentUser ==$rowRelationship["currentUser"] && $usrName == $rowRelationship["requestor_name"] &&  $rowRelationship["relationship_level"] == 1){
+              $isCurrentFriend = true;
+            }
+            else if($currentUser ==$rowRelationship["currentUser"] && $usrName == $rowRelationship["requestor_name"] &&  $rowRelationship["relationship_level"] == 1){
+              $isCurrentFriend = true;
+            }
+          }
+        }
+    
+        // if not friends add request
+    if($isCurrentFriend != true){
+          $sql = "INSERT INTO relationship_table (currentUser, requestor_name)
+                    VALUES ('$currentUser', '$requestor_name')";
+              
+        
+                if ($conn->query($sql) === TRUE) {
+                  header("location: ../index.php");
+                 } 
+                else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+              
+              $conn->close();
+    }
+
+    }
+
+
+
+    else{
+    
+      header("location: ../login.php");
+       exit();
+   }
+   
---
+++ r_v05/profile.php
@@ -0,0 +1,204 @@
+<?php
+  include_once 'header.php';
+?>
+
+<?php
+  // if there is a current session
+  if (isset($_SESSION["userName"])){
+    require_once 'include/script_db_connection.php';
+    require_once 'include/script_functions.php';
+
+    $base_friend_level = 1;
+    $best_friend_level = 2;
+    $reject_friendship = 3;
+
+    // display personal profile
+    // select all from table relay_user
+    $query = "SELECT * FROM relay_user";
+    // if results
+    if ($result = $conn->query($query)) {
+        // loop thru each row of table
+        while ($row = $result->fetch_assoc()) {
+          // if current session matches user in relay_user print profile
+          if($_SESSION["userName"] == $row["userName"] ){
+           echo '<div class="container-fluid">';
+           echo '<div class="container-sm posts shadow p-3 mb-5 bg-white rounded">';
+           echo '<div class="container title"><h3>personal profile</h3></div>';
+           echo '<div class="container content">';
+           echo '<p class="main">User Name: ' . $row["userName"] . '</p>'; 
+           echo '<p class="main">First Name: ' . $row["firstName"] . '</p>'; 
+           echo '<p class="main">Last Name: ' . $row["lastName"] . '</p>'; ;
+           echo '<p class="main">Email: ' . $row["email"] . '</p>'; 
+           echo '<p class="main">Bio: ' . $row["bio"] . '</p>'; 
+           echo '<br>  ';
+           echo '</div>';
+           echo '<div class="container footer"><p class="footer">.</p></div>';
+           echo '</div>';
+          }
+        }
+      }
+
+      // display friends list
+      echo '<div class="container-fluid">';
+      echo '<div class="container-sm posts shadow p-3 mb-5 bg-white rounded">';
+      echo '<div class="container title"><h3>friends </h3></div>';
+      // create a table with current friends
+      echo '<table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Username</th>
+          <th scope="col">Friend Level</th>
+          <th scope="col">Unfriend</th>
+        </tr>
+      </thead>
+      <tbody>';
+      $query = "SELECT * FROM relationship_table";
+      $display_friendship_level;
+      $friends_row_num = 1;
+      if ($result = $conn->query($query)) {
+          while ($row = $result->fetch_assoc()) {
+            if($row["relationship_level"] == 1 || $row["relationship_level"] == 2){
+                if($_SESSION["userName"] == $row["currentUser"]){
+                echo '<tr>';
+                echo '<th scope="row">' .$friends_row_num.' </th>';
+                echo '<td><a href="user_profile.php?usrName=' .$row["requestor_name"].'"> '.$row["requestor_name"]. '</a></td>';
+                echo '<td> '.$row["relationship_level"]. ' </td>';
+                echo '<td><a href="include/script_approve_requests.php?requestor_name=' .$row["requestor_name"].'&level=3&row=' .$row["id"].'"> remove</a></td>';
+                echo '</tr>';
+              $friends_row_num = $friends_row_num+1;
+              }
+            }
+
+           }
+        }
+      }   
+     echo '</tbody>';
+     echo '</table>';
+
+
+
+      echo '<div class="container footer"><p class="footer">.</p></div>';
+      echo '</div>';
+      echo '</div>';
+      
+      // display current friend requests
+      echo '<div class="container-fluid">';
+      echo '<div class="container-sm posts shadow p-3 mb-5 bg-white rounded">';
+      echo '<div class="container title"><h3>received requests </h3></div>';
+
+      echo '<table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Username</th>
+          <th scope="col"  colspan="2">Friend Approval Level</th>
+          <th scope="col">Reject</th>
+        </tr>
+      </thead>
+      <tbody>';
+      
+
+      $query = "SELECT * FROM relationship_table";
+      $current_reqest_row_num = 1;
+      if ($result = $conn->query($query)) {
+      
+          while ($row = $result->fetch_assoc()) {
+          
+            if($_SESSION["userName"] == $row["requestor_name"] && $row["request_status"] == 0){
+
+              echo '<tr>';
+              echo '<th scope="row">' .$current_reqest_row_num. ' </th>';
+              echo '<td><a href="user_profile.php?usrName=' .$row["currentUser"].'"> '.$row["currentUser"]. '</a></td>';
+              echo '<td><a href="include/script_approve_requests.php?requestor_name=' .$row["currentUser"].'&level=1&row=' .$row["id"].'"> Friend</a></td>';
+              echo '<td><a href="include/script_approve_requests.php?requestor_name=' .$row["currentUser"].'&level=2&row=' .$row["id"].'"> Best Friend </a></td>';
+              echo '<td><a href="include/script_approve_requests.php?requestor_name=' .$row["currentUser"].'&level=3&row=' .$row["id"].'"> Reject</a></td>';
+              echo '</tr>';
+              $current_reqest_row_num = $current_reqest_row_num+1;
+            }
+          }
+      } 
+      
+      echo '</tbody>';
+      echo '</table>';
+
+      echo '<div class="container footer"><p class="footer">.</p></div>';
+      echo '</div>';
+      echo '</div>';
+      
+      // sent requests
+      echo '<div class="container-fluid">';
+      echo '<div class="container-sm posts shadow p-3 mb-5 bg-white rounded">';
+      echo '<div class="container title"><h3>sent requests </h3></div>';
+
+      echo '<table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Username</th>
+          <th scope="col">status</th>
+        </tr>
+      </thead>
+      <tbody>';
+      
+
+      $query = "SELECT * FROM relationship_table";
+      $current_reqest_row_num = 1;
+      if ($result = $conn->query($query)) {
+      
+          while ($row = $result->fetch_assoc()) {
+          
+            if($_SESSION["userName"] == $row["currentUser"] && $row["request_status"] == 0){
+
+              echo '<tr>';
+              echo '<th scope="row">' .$current_reqest_row_num. ' </th>';
+              echo '<td><a href="user_profile.php?usrName=' .$row["requestor_name"].'"> '.$row["requestor_name"]. '</a></td>';
+
+              echo '<td><a href="include/script_approve_requests.php?requestor_name=' .$row["requestor_name"].'&level=3&row=' .$row["id"].'"> pending</a></td>';
+              echo '</tr>';
+              $current_reqest_row_num = $current_reqest_row_num+1;
+            }
+          }
+      } 
+      
+      echo '</tbody>';
+      echo '</table>';
+
+      echo '<div class="container footer"><p class="footer">.</p></div>';
+      echo '</div>';
+      echo '</div>';
+
+
+      // display personal posts
+      $query = "SELECT * FROM posts";
+
+      if ($result = $conn->query($query)) {
+      
+          while ($row = $result->fetch_assoc()) {
+          
+            if($_SESSION["userName"] == $row["usrName"]){
+              echo '<div class="container-fluid">';
+              echo '<div class="container-sm posts shadow p-3 mb-5 bg-white rounded">';
+              echo '<div class="container title"><h3>';  echo $row["usrName"]; echo " // "; echo $row["title"]; echo '</h3></div>';
+              echo '<div class="container content">';
+              echo $row["content"]; 
+              echo '<br>';
+              echo '<br>';
+              echo '</div>';
+              echo '<div class="container footer"><p class="footer">.</p></div>';
+              echo '</div>';
+              echo '</div>';
+           }
+          }
+      }       
+    
+
+  // return to index if no current session
+  else{
+    header("location: login.php");
+  }
+?>            
+
+<?php
+  include_once 'footer.php';
+?>
