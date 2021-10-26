eb23b1d93539f8a88f1fb34f16bd94dbd3eb0d84
asexton1310 <89419909+asexton1310@users.noreply.github.com>
2021-10-25 22:28:05 -0400

    updated mileston3_v5 report document and profile.php
    Changed media storage in database organization from storing media files in LAMP file system to storing media files in the database
    
    --updated profile.php to display friends list for recipients of friend requests

--- r_v06/profile.php
+++ r_v06/profile.php
@@ -74,8 +74,23 @@
                 echo '</tr>';
               $friends_row_num = $friends_row_num+1;
               }
+              else if($_SESSION["userName"] == $row["requestor_name"]){
+                echo '<tr>';
+                echo '<th scope="row">' .$friends_row_num.' </th>';
+                echo '<td><a href="user_profile.php?usrName=' .$row["currentUser"].'"> '.$row["currentUser"]. '</a></td>';
+                if($row["relationship_level"] == 2) {
+                    echo '<td> Best Friend </td>';
+                }
+                else {
+                    echo '<td> Friend </td>';
+                }
+                //echo '<td> '.$row["relationship_level"]. ' </td>';
+                echo '<td><a href="include/script_approve_requests.php?currentUser=' .$row["currentUser"].'&level=3&row=' .$row["id"].'"> remove</a></td>';
+                echo '</tr>';
+              $friends_row_num = $friends_row_num+1;
+              }
             }
-
+            
            }
         }
       }   
